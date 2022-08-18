f62add3b79f53a5ec5ee188fea70784879dd9522
Merel Van der wal <mevan-de@f1r6s4.codam.nl>
2022-08-18 15:24:14 +0200

    added a non functional cmd checker

---
+++ include/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pipex.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/08/18 13:53:16 by mevan-de      #+#    #+#                 */
+/*   Updated: 2022/08/18 14:00:02 by mevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+
+// to include execve
+# include <unistd.h>
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,70 @@
 /*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/18 13:39:26 by mevan-de      #+#    #+#                 */
-/*   Updated: 2022/08/18 13:40:20 by mevan-de      ########   odam.nl         */
+/*   Updated: 2022/08/18 14:57:56 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main(void)
+char	*get_cmd_path(char *cmd, char **envp)
 {
+	char	**paths;
+	char	*cmd_path;
+	char	*PATH_str = NULL;
+	char	*tmp;
+	int		i;
+	
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", 5))
+		{
+			PATH_str = ft_substr(envp[i], 5 , ft_strlen(envp[i] - 5));
+			if (!PATH_str)
+				return (NULL);
+			break;
+		}
+		i++;
+	}
+	ft_printf("path str = %s\n", PATH_str);
+	paths = ft_split(PATH_str, ':');
+	i = 0;
+	while (paths[i])
+	{
+		tmp = paths[i];
+		paths[i] = ft_strjoin(paths[i], "/");
+		free (tmp);
+		i++;
+	}
+	i = 0;
+	while (paths[i])
+	{
+		cmd_path = ft_strjoin(paths[i], cmd);
+		if (access(cmd_path, F_OK | X_OK) == 0) // look up f_OK and X_OK and acces
+			return (cmd_path);
+		ft_printf("cmd path = %s\n", cmd_path);
+		free (cmd_path);
+		i++;
+	}
+	exit (1);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*cmd_path;
+	char	*cmd = "ls";
+	char	*options[3] = {"ls", "-la", NULL};
+
+	(void) argc;
+	(void) argv;
+	cmd_path = get_cmd_path(cmd, envp);
+	if (!cmd_path)
+	{
+		perror(cmd_path);
+		return (-1);
+	}
+	ft_printf("cmd path = %s\n", cmd_path);
+	execve(cmd_path, options, envp);
 	return (0);
 }
